679cad719bc98e922699c299b1354d580e9d0282
LindsayH <lindsaynhenderson@gmail.com>
2022-12-18 14:22:08 -0600

    new html form with php file

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,23 +1,48 @@
 export const ContactForm = () => {
-  
-  return(
 
-<form method="POST" action="/https://formspree.io/f/xoqzajpp" enctype="multipart/form-data">
-  <label for="name">Full Name</label>
-  <input id="name" type="text" name="name" 
-  // pattern="[A-Z//a-z]+" 
-  required/>
-
-  <label for="email">Email Address</label>
-  <input id="email" type="email" name="email" required/>
-
-  <label for="message">Message</label>
-  <textarea id="message" name="message" required></textarea>
-
-  <button type="submit">Submit</button>
-</form>
-
+return(
+<>
+    <h3>Contact us</h3>
+    <form id="fcf-form-id" class="fcf-form-class" method="post" action="contact-form-process.php">
+      <label for="Name" class="fcf-label">Your name</label>
+      <div class="fcf-input-group">
+        <input type="text" id="Name" name="Name" class="fcf-form-control" required/>
+      </div>
+      <div class="fcf-form-group">
+        <label for="Email" class="fcf-label">Your email address</label>
+        <div class="fcf-input-group">
+          <input type="email" id="Email" name="Email" class="fcf-form-control" required/>
+        </div>
+        <div class="fcf-form-group">
+          <label for="Message" class="fcf-label">Your message</label>
+          <div class="fcf-input-group">
+            <textarea id="Message" name="Message" class="fcf-form-control" rows="6" maxlength="3000" required></textarea>
+          </div>
+        </div>
+        <div class="fcf-form-group">
+          <button type="submit" id="fcf-button" class="fcf-btn fcf-btn-primary fcf-btn-lg fcf-btn-block">Send</button>
+        </div>
+      </div>
+    </form>
+</>
 )}
+
+
+// <form method="POST" action="/https://formspree.io/f/xoqzajpp" enctype="multipart/form-data">
+//   <label for="name">Full Name</label>
+//   <input id="name" type="text" name="name"  
+//   // pattern="[A-Z//a-z]+" 
+//   required/>
+
+//   <label for="email">Email Address</label>
+//   <input id="email" type="email" name="email" required/>
+
+//   <label for="message">Message</label>
+//   <textarea id="message" name="message" required></textarea>
+
+//   <button type="submit">Submit</button>
+// </form>
+
 
 // import React from 'react';
 // import { useForm, ValidationError } from '@formspree/react';
