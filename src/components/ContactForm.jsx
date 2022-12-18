87ded3e6c0985d745fbb68f0b210e3b5ce24884a
LindsayH <lindsaynhenderson@gmail.com>
2022-12-18 13:55:34 -0600

    testing different code for form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,22 +1,61 @@
 
-export const ContactForm = () => {
+// export const ContactForm = () => {
   
-  return(
+//   return(
 
-<form method="POST" action="/https://formspree.io/f/xoqzajpp">
-  <label for="name">Full Name</label>
-  <input id="name" type="text" name="name" 
-  // pattern="[A-Z//a-z]+" 
-  required/>
+// <form method="POST" action="/https://formspree.io/f/xoqzajpp">
+//   <label for="name">Full Name</label>
+//   <input id="name" type="text" name="name" 
+//   // pattern="[A-Z//a-z]+" 
+//   required/>
 
-  <label for="email">Email Address</label>
-  <input id="email" type="email" name="email" required />
+//   <label for="email">Email Address</label>
+//   <input id="email" type="email" name="email" required/>
 
-  <label for="message">Message</label>
-  <textarea id="message" name="message" required></textarea>
+//   <label for="message">Message</label>
+//   <textarea id="message" name="message" required></textarea>
 
-  <button type="submit">Submit</button>
-</form>
+//   <button type="submit">Submit</button>
+// </form>
 
-)}
+// )}
+
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+
+export const ContactForm = () =>  {
+  const [state, handleSubmit] = useForm("https://formspree.io/f/xoqzajpp");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
+  return (
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
+  );
+}
 
