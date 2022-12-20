7542306fccc4204b36100c730513ae244d128610
LindsayH <lindsaynhenderson@gmail.com>
2022-12-20 17:59:20 -0600

    carousel slides are rotating

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {Main} from './components/Main/Main'
 
 
 
+
 function App() {
   return (
     <div className="App">
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import {Nav} from '../NavBar/NavBar'
 import {About} from '../About'
 import {Process} from '../Process'
 import {ContactForm} from '../ContactForm';
+import { Portraits } from '../Portraits';
 
 
 export const Main = (props) => {
@@ -9,6 +10,7 @@ export const Main = (props) => {
     <div>
       <Nav />
       <About />
+      <Portraits/>
       <Process />
       <ContactForm/>
     </div>
