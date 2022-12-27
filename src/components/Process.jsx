8b507b43870ed41e44b2ff2cec0997b581e04e96
LindsayH <lindsaynhenderson@gmail.com>
2022-12-27 13:53:47 -0600

    adjust text

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -10,7 +10,7 @@ export const Process = () => {
         alt="rough draft of a black and white cate sitting in palm leaves" /> */}
         <p className="p2">I take a $25 deposit, and then will paint a rough draft of the painting and send it to you for approval.  Upon approval, I will paint the final portrait and send you a photo. After that, I collect the remaining cost of the painting, put your original artwork in a matboard for extra protection, and pop it in the mail!</p>
         {/* <img src="/paintings/spookyFinal.jpg" alt="a black and white cate sitting in palm leaves" /> */}
-        <p className="p3">The whole process takes about 6-8 weeks.  I can try to accomodate for a shorter time window on a case by case basis. It never hurts to ask!</p>
+        <p className="p3">The whole process takes about 6-8 weeks.</p>
       </div>
     </div>
   )
