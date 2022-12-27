e3d39c5aa6f75d78b34f78f3d97730ab1792bf3a
LindsayH <lindsaynhenderson@gmail.com>
2022-12-27 15:01:06 -0600

    reload images in photos folder

--- src/components/Portraits.jsx
+++ src/components/Portraits.jsx
@@ -1,22 +1,22 @@
 
-// export const Portraits= () => {
+export const Portraits= () => {
 
-//   return (
-//     <section >
-//       <div id="myCarousel" className="carousel slide carousel-fade" data-bs-ride="carousel">
-//         <div className="carousel-inner" role="listbox">
-//         <div className="carousel-item active">
-//           <img src="/paintings/pooldog.jpg" className="d-block w-100" alt="..."/>
-//         </div> 
-//         <div className="carousel-item">
-//           <img src="/paintings/sleeping.jpg" className="d-block w-100" alt="..."/>
-//         </div> 
-//         <div className="carousel-item">
-//           <img src="/paintings/seaturtle.jpg" className="d-block w-100" alt="..."/>
-//         </div>
-//       </div>
-//     </div>
-//     </section>
+  return (
+    <section >
+      <div id="myCarousel" className="carousel slide carousel-fade" data-bs-ride="carousel">
+        <div className="carousel-inner" role="listbox">
+        <div className="carousel-item active">
+          <img src="paintings/pooldog.jpg" className="d-block w-100" alt="..."/>
+        </div> 
+        <div className="carousel-item">
+          <img src="paintings/sleeping.jpg" className="d-block w-100" alt="..."/>
+        </div> 
+        <div className="carousel-item">
+          <img src="paintings/seaturtle.jpg" className="d-block w-100" alt="..."/>
+        </div>
+      </div>
+    </div>
+    </section>
 
   )
   }
