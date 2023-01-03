6a43c6cd7ba4ecb20bfe5d79104770ac709c7302
LindsayH <lindsaynhenderson@gmail.com>
2023-01-03 17:13:15 -0600

    edit content for ordering page

--- src/components/Ordering.jsx
+++ src/components/Ordering.jsx
@@ -4,23 +4,23 @@ export const  Ordering = ()=> {
     <section className="ordering">
       <div className="size-price">
         <div className="numbers">
-        <h2>9X12 Original Watercolor</h2>
-        <h3>comes matted in 11X14 inch board <br/> ready to frame</h3>
-        <div className="price"><h2>$175</h2>
-        <h3>Free shipping within United States</h3>
+        <h1>9X12 Original Watercolor</h1>
+        <h4>comes matted in 11X14 inch matboard <br/> ready to frame</h4>
+        <div className="price"><h4>$175</h4>
         </div>
         </div>
         <div>
       <h1>Process for ordering</h1>
       <div className="process-explanation">
         <h2>Email @paintallthedogs@gmail.com</h2>
-        <p className="p1">I base my painting on one of your favorite photos of your pet. Please send me the photo you would like me to use, along with any other photos that might be a good reference for their markings, or personality. I love hearing about each special animal. </p>
+        <p className="p1">In order to capture you pet's personality as much as I can, I base my painting on one of your favorite photos of your pet. Send me the photo you would like me to use, along with any other photos that might be a good reference for their markings, or personality. I love hearing about each special animal. </p>
 
 
-        <p className="p2"> When the portrait is completed, I will send you an email with a digital photo of the painting and confirm your mailing address.  Your painting will be shipped in a mat so it's ready to frame. I will provide you with a tracking number.</p>
+        <p className="p2"> When the portrait is completed, I will send you an email with a digital photo of the painting and confirm your mailing address.  Your painting will be shipped ready to frame in a while 11x14 matboard. I will provide you with a tracking number.</p>
         <img src='/paintings/spookyFinal.jpg' alt="a black and white cat sitting in palm leaves" />
         <p className="p3">The whole process takes about 6-8 weeks.</p>
       </div>
+        <h3>Free shipping within United States</h3>
     </div>
       </div>
     </section>
