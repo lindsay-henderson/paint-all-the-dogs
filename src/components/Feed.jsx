3b9f5725e244d37b90054ff6805f72cbf304dd97
LindsayH <lindsaynhenderson@gmail.com>
2023-01-04 17:02:49 -0600

    adjust text above portrait carousel

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -12,7 +12,7 @@ export const Feed = () => {
     <h3>Collab ideas?</h3> 
     <h3>Questions?</h3> 
     <h3>Cookie recipes?</h3>
-    <Button className='button' variant="contained" size='large'><a href="mailto:paintallthedogs@gmail.com?subject=subject&body=content"><h4>Email Me</h4></a></Button>
+    <Button className='button' variant="contained" size='large'><a href="mailto:paintallthedogs@gmail.com?subject=subject&body=content"><p>Email Me</p></a></Button>
   </div>
   <div id='instabox'>
     <InstagramEmbed url="https://www.instagram.com/p/CQUZZw_FqeX/?utm_source=ig_web_copy_link"/>
