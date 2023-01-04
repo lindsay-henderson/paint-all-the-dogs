cdeb34ec74adcc46348f825cba8f39ade25aec45
LindsayH <lindsaynhenderson@gmail.com>
2023-01-04 15:42:25 -0600

    add email button to feed section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export const About = () => {
     <div className="about" id="about">
       <div className='about-me'>
       <h2>Hi! I'm Lindsay.</h2>
-        <p>I am a lifelong artist and animal lover.  I began painting pets because I wanted to give creative gifts to my friends and family that were personal and meaningful. Now, through my business, I have the privilege of learning about  even more special animals. My goal as an artist is, and has always been, to bring happiness through my work. </p>
+        <p>I am a lifelong artist and animal lover.  I began painting pets because I wanted to give creative gifts to my friends and family that were personal and meaningful. Over the past few years I have the privilege of learning about  many special animals. My goal as an artist is, and has always been, to bring happiness through my work. </p>
       </div>
       <div className='about-me'>
       <img src="/menrusty.JPG" alt="Lindsay next to a white dog" />
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,15 +1,19 @@
 import '../styles/feed.css'
 import { InstagramEmbed } from 'react-social-media-embed'
+import { Button } from '@mui/material'
 
 export const Feed = () => {
 
   return(
 
 <div id='feed-info'>
-  <div>
-  <p>Follow me on Instagram for more cuteness</p>
+  <div id='stay-in-touch'>
+    <h2>Let's stay in touch</h2>
+    <h3>Collab ideas?</h3> 
+    <h3>Questions?</h3> 
+    <h3>Cookie recipes?</h3>
+    <Button className='button' variant="contained" size='large'><a href="mailto:{paintallthedogs@gmail.com}?subject={subject}&body={content}"><h4>Email Me</h4></a></Button>
   </div>
-
   <div id='instabox'>
     <InstagramEmbed url="https://www.instagram.com/p/CQUZZw_FqeX/?utm_source=ig_web_copy_link"/>
   </div>
