5a1a0ac6a4a595ed2e2b2d259ac2d2ced1c6fef0
LindsayH <lindsaynhenderson@gmail.com>
2023-02-15 11:40:50 -0600

    add cat portrait to carousel git push origin main

--- src/components/Portraits.jsx
+++ src/components/Portraits.jsx
@@ -5,7 +5,7 @@ import 'aos/dist/aos.css';
 export const Portraits= () => {
 
   return (
-    <section >
+    <section>
       <h3 id='intro-text'>Watercolor Pet Portraits</h3>
       <div id="myCarousel" className="carousel slide carousel-fade" data-bs-ride="carousel">
         <div className="carousel-inner" role="listbox">
@@ -21,12 +21,16 @@ export const Portraits= () => {
           </div> 
           <div className="carousel-item">
             <div className='img-frame'>
+              <img src="paintings/brown-cat.jpg" className="d-block w-100" alt="..."/>
+            </div> 
+          </div>
+          <div className="carousel-item">
+            <div className='img-frame'>
               <img src="paintings/seaturtle.jpg" className="d-block w-100" alt="..."/>
             </div>
           </div>
         </div>
       </div>
     </section>
-
   )
   }
